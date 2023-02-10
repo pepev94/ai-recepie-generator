0a00741c569c783d72ec330c245e819df4de4f30
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-02-10 11:41:18 -0600

    removed extra code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import CreateRecipie from "@/components/CreateRecipie";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
