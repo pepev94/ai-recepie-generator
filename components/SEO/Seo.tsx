63e33246f5a18cd41281e28d2002a5766c119086
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-02-10 15:15:12 -0600

    added seo and gtag

---
+++ components/SEO/Seo.tsx
@@ -0,0 +1,73 @@
+import Head from "next/head";
+
+const DOMAIN = "http://aifoodie.co/";
+const DEFAULT_OG_IMAGE =
+  "https://ai-recepie-generator-353vm5epk-pepev94.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAiChef.708d2e4b.png&q=75&w=640";
+
+export default function Seo({
+  title = "Create Recipies powered by AI",
+  description = "Create any recipie powered by AI",
+  siteName = "AI Chef",
+  canonical = DOMAIN,
+  ogImage = DEFAULT_OG_IMAGE,
+  ogType = "website",
+  //   twitterHandle = "@d__raptis",
+}) {
+  return (
+    <Head>
+      <title key="title">{`${title} – ${siteName}`}</title>
+      <meta name="description" content={description} />
+      <meta key="og_type" property="og:type" content={ogType} />
+      <meta key="og_title" property="og:title" content={title} />
+      <meta
+        key="og_description"
+        property="og:description"
+        content={description}
+      />
+      <meta key="og_locale" property="og:locale" content="en_IE" />
+      <meta key="og_site_name" property="og:site_name" content={siteName} />
+      <meta key="og_url" property="og:url" content={canonical ?? DOMAIN} />
+      <meta key="og_site_name" property="og:site_name" content={siteName} />
+      <meta
+        key="og_image"
+        property="og:image"
+        content={ogImage ?? DEFAULT_OG_IMAGE}
+      />
+      <meta
+        key="og_image:alt"
+        property="og:image:alt"
+        content={`${title} | ${siteName}`}
+      />
+      <meta key="og_image:width" property="og:image:width" content="1200" />
+      <meta key="og_image:height" property="og:image:height" content="630" />
+
+      <meta name="robots" content="index,follow" />
+
+      <meta
+        key="twitter:card"
+        name="twitter:card"
+        content="summary_large_image"
+      />
+      {/* <meta 
+        key="twitter:site" 
+        name="twitter:site" 
+        content={twitterHandle} 
+      />
+      <meta
+        key="twitter:creator"
+        name="twitter:creator"
+        content={twitterHandle}
+      /> */}
+      <meta key="twitter:title" property="twitter:title" content={title} />
+      <meta
+        key="twitter:description"
+        property="twitter:description"
+        content={description}
+      />
+
+      <link rel="canonical" href={canonical ?? DOMAIN} />
+
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import NavBar from "@/components/navBar";
 import { ThemeProvider } from "@emotion/react";
 import { MuiTheme } from "@/utils/theme";
 import { CssBaseline } from "@mui/material";
+import Script from "next/script";
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
@@ -26,14 +27,34 @@ const App = (props: AppProps) => {
   }, [shortLocale]);
 
   return (
-    <IntlProvider locale={shortLocale} messages={messages} onError={() => null}>
-      <ThemeProvider theme={MuiTheme}>
-        <CssBaseline />
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-9RQ9TBFXW6"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-        <NavBar></NavBar>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </IntlProvider>
+          gtag('config', 'G-9RQ9TBFXW6');
+        `}
+      </Script>
+
+      <IntlProvider
+        locale={shortLocale}
+        messages={messages}
+        onError={() => null}
+      >
+        <ThemeProvider theme={MuiTheme}>
+          <CssBaseline />
+
+          <NavBar></NavBar>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </IntlProvider>
+    </>
   );
 };
 export default App;
