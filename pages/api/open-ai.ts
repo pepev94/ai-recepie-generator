262b71bf13fc4e4ae17cba93dab5c40d881ec292
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-02-10 13:50:04 -0600

    fixed spanish

--- pages/api/open-ai.ts
+++ pages/api/open-ai.ts
@@ -40,22 +40,22 @@ const getPromt = (body: BodyGetOpenAiResult) => {
     personCount,
   } = body;
   switch (selectedLanguage) {
-    case LanguagesEnum.en:
-      return `Quiero un receta de cocina. Dame el titulo, la lista de ingredientes, el paso a paso para preparar. Se especifico en los detalles de la preparación, agrega la información nutrional al final de lo siguiente: Una comida casera tipo ${foodType} ,f´cil de hacer, que tarde menos de 30 min en hacer.${
+    case LanguagesEnum.es:
+      return `Responde en español los siguiente: Quiero un receta de cocina. Dame el titulo, la lista de ingredientes, el paso a paso para preparar. Se especifico en los detalles de la preparación, agrega la información nutrional al final de lo siguiente: Una comida casera tipo ${foodType} ,f´cil de hacer, que tarde menos de 30 min en hacer.${
         countMacros
           ? `Que tenga menos de ${targetCarbs} carbohidratos por porción, y  ${targetProtein} gramos de proteina  por porción`
           : ""
       }. ${
         alergies !== "" ? `Soy alergico a ${alergies}` : ""
-      } Quiero que los ingredientes principales sean ${primaryIngredient}. Debe de ser para ${personCount} personas. Responde en español`;
-    case LanguagesEnum.es:
+      } Quiero que los ingredientes principales sean ${primaryIngredient}. Debe de ser para ${personCount} personas.`;
+    case LanguagesEnum.en:
       return `Give me the title, list of ingredients and step by step process to prepare it, Be specific on the details of the process, add the nutritional information at the en of the the following: A ${foodType} homemade cuisine, easy to do, takes less than 30 minutes to prepare. ${
         countMacros
           ? `It must have less ${targetCarbs} calories per serve, with a target protein of ${targetProtein} gms per serve`
           : ""
       }. ${
         alergies !== "" ? `I am allergic to ${alergies}` : ""
-      } And I want the main ingredient to be ${primaryIngredient}. It mus be for ${personCount} persons`;
+      } And I want the main ingredient to be ${primaryIngredient}. It must be for ${personCount} persons`;
   }
 };
 //
@@ -64,6 +64,8 @@ const handler = async (req: Request): Promise<Response> => {
   const body = (await req.json()) as BodyGetOpenAiResult;
 
   const prompt = getPromt(body);
+
+  console.log(body, prompt);
 
   const payload = {
     model: "text-davinci-003",
