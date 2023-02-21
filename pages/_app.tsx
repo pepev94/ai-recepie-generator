ab9f0d13ac800296b5760438f85c2eac13abccee
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-02-21 12:35:56 -0600

    added hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,6 +44,18 @@ const App = ({ Component, pageProps }: AppProps) => {
         src="https://www.googletagmanager.com/gtag/js?id=G-9RQ9TBFXW6"
         strategy="afterInteractive"
       />
+      <Script id="hot-jar" strategy="afterInteractive">
+        {`
+           (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3377353,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+      </Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
