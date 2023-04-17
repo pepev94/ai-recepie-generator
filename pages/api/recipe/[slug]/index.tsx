384a7ae13bca871063ef1723a090008b92b934d6
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-04-17 14:38:48 -0500

    fixed console logs and views update

--- pages/api/recipe/[slug]/index.tsx
+++ pages/api/recipe/[slug]/index.tsx
@@ -2,8 +2,6 @@ import mongooseConnect from "@/lib/mongooseConnect";
 import Recepie from "@/models/Recepie";
 
 import { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]";
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,11 +16,11 @@ export default async function handler(
       try {
         const { slug } = req.query;
         const recepies = await Recepie.find({ slug });
-        res.status(200).json({ data: recepies });
         const recepie = recepies[0];
-        Recepie.findByIdAndUpdate(recepie.id, {
+        await Recepie.findByIdAndUpdate(recepie.id, {
           $set: { views: (recepie?.views || 0) + 1 },
         }).exec();
+        res.status(200).json({ data: recepies });
       } catch (error) {
         res.status(400).json({ success: false });
       }
--- pages/recipe/[slug].tsx
+++ pages/recipe/[slug].tsx
@@ -19,11 +19,8 @@ export default function Recipe({ recepie }: { recepie: RecepieModel }) {
 
 export async function getStaticPaths() {
   const allSlugs = await getAllSlugs();
-  console.log("allSlugsdebugx", allSlugs);
   const cleanedSlugs = allSlugs.filter(Boolean);
   // .filter((slug: string) => slug.length < 50);
-
-  console.log("allSlugs", cleanedSlugs.length);
 
   const paths = {
     paths: cleanedSlugs.map((slug: any) => {
@@ -41,7 +38,6 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: { params: { slug: string } }) {
   const recepie = await getRecepieBySlug(params.slug);
-  console.log("recepie", recepie);
 
   return {
     props: {
