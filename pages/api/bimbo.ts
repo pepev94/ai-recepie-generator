e2de9f5387f076c41592b441b4f441f46089c4eb
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-05-30 14:13:22 -0500

    added bimbo

---
+++ pages/api/bimbo.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const getBimboSKU = () => {
+  const randomNum = `${Math.floor(Math.random() * 15) + 1}`;
+  if (randomNum.length === 2) return `BI00${randomNum}`;
+  return `BI000${randomNum}`;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const numOfItems = Math.floor(Math.random() * 5) + 1;
+  const arrayOfItems = [];
+  for (let i = 0; i < numOfItems; i++) {
+    arrayOfItems.push(getBimboSKU());
+  }
+
+  res.status(200).json({ nboSKUs: [...new Set(arrayOfItems)] });
+}
