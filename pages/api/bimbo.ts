433c300f42de2c344560a136f73724c8d6e8e918
pepev94 <60986337+pepev94@users.noreply.github.com>
2023-10-12 13:32:22 -0600

    added prefix and max num

--- pages/api/bimbo.ts
+++ pages/api/bimbo.ts
@@ -1,16 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const getBimboSKU = () => {
+const getSKU = (prefix: string) => {
   const randomNum = `${Math.floor(Math.random() * 15) + 1}`;
-  if (randomNum.length === 2) return `BI00${randomNum}`;
-  return `BI000${randomNum}`;
+  if (randomNum.length === 2) return `${prefix}00${randomNum}`;
+  return `${prefix}000${randomNum}`;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const numOfItems = Math.floor(Math.random() * 5) + 4;
+  const { prefix, maxNum } = req.query;
+
+  // Ensure maxNum is an integer and is greater than or equal to 3
+  const parsedMaxNum = Number(maxNum);
+  const validMaxNum =
+    !isNaN(parsedMaxNum) && parsedMaxNum >= 3 ? parsedMaxNum : 4;
+
+  // Generate a random number between 3 (inclusive) and validMaxNum (inclusive)
+  let numOfItems = Math.floor(Math.random() * (validMaxNum - 3 + 1)) + 3;
+  console.log(numOfItems);
+  if (numOfItems < 3) numOfItems = 3;
   const arrayOfItems = [];
   for (let i = 0; i < numOfItems; i++) {
-    arrayOfItems.push(getBimboSKU());
+    arrayOfItems.push(getSKU(prefix ? String(prefix) : "PRO"));
   }
 
   res.status(200).json({ nboSKUs: [...new Set(arrayOfItems)] });
